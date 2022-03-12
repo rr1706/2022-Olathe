a31bd00df0e1f81a61fa8dd8a073ead3f5063f50
Brady Schindler <bradyhschindler2305@gmail.com>
2022-03-12 15:08:14 -0600

    Update Autos

---
+++ src/main/java/frc/robot/commands/Autos/EMERGENCYNOBALL.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.Autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Utilities.AutoFromPathPlanner;
+import frc.robot.commands.FeedShooter;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.ShooterHood;
+import frc.robot.subsystems.Turret;
+import frc.robot.subsystems.Swerve.Drivetrain;
+
+public class EMERGENCYNOBALL extends SequentialCommandGroup {
+
+    public EMERGENCYNOBALL(Drivetrain drivetrain, Intake leftIntake, Intake rightIntake, Elevator bottom, Elevator top, Turret turret, ShooterHood hood, Shooter shooter, Climber climb){
+        final AutoFromPathPlanner fiveBallUno = new AutoFromPathPlanner(drivetrain, "2022-EMERGENCYNOBALL", 3.2);
+        
+
+        addCommands(fiveBallUno);
+    }
+        
+}
